a4c66061f5eed5d27a10a8cc60819174b60f953d
Ryan <rmf919@live.com>
2020-09-29 15:48:50 +1000

    Add project files.

---
+++ GoFish/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace GoFish
+{
+    partial class GoFish
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.yourHand = new System.Windows.Forms.ListBox();
+            this.buttonAsk = new System.Windows.Forms.Button();
+            this.yourName = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textProgress = new System.Windows.Forms.TextBox();
+            this.textBooks = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // yourHand
+            // 
+            this.yourHand.FormattingEnabled = true;
+            this.yourHand.Location = new System.Drawing.Point(481, 32);
+            this.yourHand.Name = "yourHand";
+            this.yourHand.Size = new System.Drawing.Size(120, 459);
+            this.yourHand.TabIndex = 0;
+            // 
+            // buttonAsk
+            // 
+            this.buttonAsk.Enabled = false;
+            this.buttonAsk.Location = new System.Drawing.Point(481, 503);
+            this.buttonAsk.Name = "buttonAsk";
+            this.buttonAsk.Size = new System.Drawing.Size(120, 23);
+            this.buttonAsk.TabIndex = 1;
+            this.buttonAsk.Text = "Ask for a card";
+            this.buttonAsk.UseVisualStyleBackColor = true;
+            // 
+            // yourName
+            // 
+            this.yourName.Location = new System.Drawing.Point(12, 32);
+            this.yourName.Name = "yourName";
+            this.yourName.Size = new System.Drawing.Size(241, 20);
+            this.yourName.TabIndex = 2;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(9, 16);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(58, 13);
+            this.nameLabel.TabIndex = 3;
+            this.nameLabel.Text = "Your name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(478, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(259, 32);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(216, 23);
+            this.buttonStart.TabIndex = 5;
+            this.buttonStart.Text = "Start the game!";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Game progress";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 377);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Books";
+            // 
+            // textProgress
+            // 
+            this.textProgress.Location = new System.Drawing.Point(12, 71);
+            this.textProgress.Multiline = true;
+            this.textProgress.Name = "textProgress";
+            this.textProgress.ReadOnly = true;
+            this.textProgress.Size = new System.Drawing.Size(463, 303);
+            this.textProgress.TabIndex = 9;
+            // 
+            // textBooks
+            // 
+            this.textBooks.Location = new System.Drawing.Point(15, 393);
+            this.textBooks.Multiline = true;
+            this.textBooks.Name = "textBooks";
+            this.textBooks.ReadOnly = true;
+            this.textBooks.Size = new System.Drawing.Size(460, 133);
+            this.textBooks.TabIndex = 10;
+            // 
+            // GoFish
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 546);
+            this.Controls.Add(this.textBooks);
+            this.Controls.Add(this.textProgress);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.yourName);
+            this.Controls.Add(this.buttonAsk);
+            this.Controls.Add(this.yourHand);
+            this.Name = "GoFish";
+            this.Text = "Go Fish";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox yourHand;
+        private System.Windows.Forms.Button buttonAsk;
+        private System.Windows.Forms.TextBox yourName;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textProgress;
+        private System.Windows.Forms.TextBox textBooks;
+    }
+}
+
